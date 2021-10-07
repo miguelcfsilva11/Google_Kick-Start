e832c2957b829c9f67859b1ee86eb4c4ac9dd99b
Miguel Silva <miguelcfsilva@gmail.com>
2021-10-07 15:31:39 +0100

    Solved Arithmetic Square

---
+++ 2021 Edition/Round D/Arithmetic.py
@@ -0,0 +1,67 @@
+def arithmetic(grid):
+
+    possible_missing = []
+    
+    if (grid[2][2] + grid[0][0]) % 2 == 0:
+        possible_missing.append((grid[2][2] + grid[0][0])// 2)
+    if (grid[0][1] + grid[2][1]) % 2 == 0:
+        possible_missing.append((grid[0][1] + grid[2][1])// 2)
+    if (grid[1][0] + grid[1][2]) % 2 == 0:
+        possible_missing.append((grid[1][0] + grid[1][2])// 2)
+    if (grid[0][2] + grid[2][0]) % 2 == 0:
+        possible_missing.append((grid[0][2] + grid[2][0])// 2)
+    
+    
+    counter = 0
+    max_counter = 0
+    
+    for i in possible_missing:
+        grid[1][1] = i
+        
+        if grid[0][0] + grid[0][2] == grid[0][1] * 2:
+            counter += 1
+        if grid[1][0] + grid[1][2] == grid[1][1] * 2:
+            counter += 1
+        if grid[2][0] + grid[2][2] == grid[2][1] * 2:
+            counter += 1
+            
+        if grid[0][0] + grid[2][0] == grid[1][0] * 2:
+            counter += 1
+        if grid[0][1] + grid[2][1] == grid[1][1] * 2:
+            counter += 1
+        if grid[0][2] + grid[2][2] == grid[1][2] * 2:
+            counter += 1    
+        
+        if grid[0][0] + grid[2][2] == grid[1][1] * 2:
+            counter += 1
+        if grid[2][0] + grid[0][2] == grid[1][1] * 2:
+            counter += 1
+        
+        if counter > max_counter:
+            max_counter = counter
+        counter = 0
+    return max_counter
+    
+t = int(input())
+counter = 1
+
+while t > 0:
+    
+    grid = []
+    row = []
+    
+    col = 0
+    while len(grid) < 3:
+
+        el = list(map(int, input().split()))
+
+        if len(el) == 2:
+            last_el = el[-1]
+            el.pop()
+            el.append(1)
+            el.append(last_el)
+        
+        grid.append(el)
+    t -= 1
+    counter += 1
+    print('Case #{0}: '.format(counter - 1) + str(arithmetic(grid)))
